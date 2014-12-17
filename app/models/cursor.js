307757719d8c10ab7db89e1dc20b98c7197d7faa
posadchiy <posadchiy@gmail.com>
2014-12-17 03:57:08 +0200

    Introduce cursor

---
+++ app/models/cursor.js
@@ -0,0 +1,7 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  isPlaying: DS.attr('boolean'),
+  isMuted: DS.attr('boolean'),
+  youtubeVideo: DS.belongsTo('video')
+});
--- app/routes/application.js
+++ app/routes/application.js
@@ -5,12 +5,27 @@ export default Ember.Route.extend({
     return this.store.find('video');
   },
   actions: {
-    addVideo: function () {
-      console.log('wat');
-      var video = this.store.createRecord('video',{
-        youtubeId: 'gKiaLSJW5xI'
+    addVideo: function(id) {
+      var self = this;
+      this.store.find('video').then(function(videos){
+        self._createVideo(id).then(function(video){
+          //if (videos.length === 0){
+            self._createCursor(video);
+          //}
+        });
       });
-      video.save();
     }
+  },
+
+  _createVideo: function(id){
+    return this.store.createRecord('video',{
+      youtubeId: 'XN5TG3kUoNE'
+    }).save();
+  },
+
+  _createCursor: function(video){
+    return this.store.createRecord('cursor',{
+      youtubeVideo: video
+    }).save();
   }
 });
